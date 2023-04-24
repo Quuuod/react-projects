1c1c0ef507898b1cdee950b5a84d105d2d1af26f
Quuuod <prizraklogan@gmail.com>
2023-04-24 10:58:17 +0300

    react educational projects

---
+++ 05-react-JSX-green-btn/script.js
@@ -0,0 +1,20 @@
+const App = ({ initialButtonText, initialClassName }) => {
+  const [buttonText, setBtnText] = React.useState(initialButtonText)
+  const [classesList, setClassesList] = React.useState(initialClassName)
+
+  const onButtonClick = () => {
+    setBtnText('Hello')
+    setClassesList('green-btn')
+  }
+
+  return (
+    <div className="app">
+      <button className={classesList} onClick={onButtonClick}>
+        {buttonText}
+      </button>
+    </div>
+  )
+}
+const container = document.querySelector('#app')
+const root = ReactDOM.createRoot(container)
+root.render(<App initialButtonText="Click me" initialClassName="" />)
